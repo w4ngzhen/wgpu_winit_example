fb373ccabd4ac42dd2151b0c254f23bccd8a8e7c
w4ngzhen <w4ngzhen@hotmail.com>
2024-05-17 09:02:39 +0800

    feat: add chapter_00: a simple winit.

---
+++ ch00_simple_winit/README.md
@@ -0,0 +1,3 @@
+# chapter_00
+
+a simple winit example base winit v0.30
---
+++ ch00_simple_winit/src/app.rs
@@ -0,0 +1,37 @@
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::ActiveEventLoop;
+use winit::window::{Window, WindowId};
+
+#[derive(Default)]
+pub struct App {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        if self.window.is_none() {
+            let win_attr = Window::default_attributes().with_title("winit example");
+            let window = event_loop
+                .create_window(win_attr)
+                .expect("create window err.");
+            self.window = Some(window);
+        }
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        _window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        match event {
+            WindowEvent::CloseRequested => {
+                // macOS err: https://github.com/rust-windowing/winit/issues/3668
+                // This will be fixed as winit 0.30.1.
+                event_loop.exit();
+            }
+            _ => (),
+        }
+    }
+}
